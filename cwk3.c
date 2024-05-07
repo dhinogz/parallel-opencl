351a961c1ee1ecb30e615ff9067357028e1dbfbe
dhinogz <davidhinojosagzz@gmail.com>
2024-05-07 11:45:07 +0100

    Bug fix

--- cwk3.c
+++ cwk3.c
@@ -95,7 +95,7 @@ int main( int argc, char **argv )
 		&maxWorkItems,
 		NULL
 	);
-	size_t maxWorkItemsND[2] = {N, M};
+	size_t maxWorkItemsND[2] = {maxWorkItems/N, maxWorkItems/M};
 
 	// INFO: DELETE Ensure that local work size divides evenly into global size 
 	while (globalSize[0] % maxWorkItemsND[0] != 0 || globalSize[1] % maxWorkItemsND[1] != 0) {
